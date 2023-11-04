dabf41a340170d9b326cca81d72f865ad1118f4b
JSCodeur1 <138124682+JSCodeur1@users.noreply.github.com>
2023-11-04 20:50:39 +0100

    Main.java
    Main file

---
+++ snake/Main.java
@@ -0,0 +1,31 @@
+package snake;
+
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class Main {
+	
+	public JLabel l = new JLabel("Score : ");
+	
+	public static void main(String[] args) {
+			
+	  JFrame f = new JFrame();
+    ClicTest k = new ClicTest();
+    Jeu jeu = new Jeu();
+    
+    f.setSize(1600, 870);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.add(jeu);
+    f.setVisible(true);
+    f.addKeyListener(k);
+    
+    Timer timer = new Timer(0, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            jeu.setDir(k.dirs.dir);
+        }
+    });
+    timer.start();
+	}	
+}
